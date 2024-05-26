2d11674e64fe3429fa0a56e0840af62c8a6c2a0a
EmmanuelMess <emmanuelbendavid@gmail.com>
2024-05-26 17:08:10 -0300

    Remove pygame dependency of Drawer

--- code/simulator/drawers/Drawer.py
+++ code/simulator/drawers/Drawer.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
-import pygame
 
 
 class Drawer(ABC):
@@ -9,7 +8,7 @@ class Drawer(ABC):
     Allows something to be drawn on a surface
     """
     @abstractmethod
-    def draw(self, surface: pygame.Surface, origin: np.ndarray) -> None:
+    def draw(self, surface: 'pygame.Surface', origin: np.ndarray) -> None:
         pass
 
     @abstractmethod
