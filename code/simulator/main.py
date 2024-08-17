6323680fcc02ff7416a99819be59eccab92f50a4
EmmanuelMess <emmanuelbendavid@gmail.com>
2024-08-17 13:19:41 -0300

    Better arguments

--- code/simulator/main.py
+++ code/simulator/main.py
@@ -40,11 +40,11 @@ def runWithUi(simulation: Simulation, ui: UI, fileSaver: Union[FileSaver, None],
         lastFrame = timer()
 
 
-def runNoUi(simulation: Simulation, fileSaver: FileSaver, timestep: np.float64, steps: int) -> None:
-    for i in range(0, steps):
+def runNoUi(simulation: Simulation, fileSaver: FileSaver, timestep: np.float64, iterations: int) -> None:
+    for i in range(0, iterations):
         simulation.update(timestep)
 
-        print(f"\r{i+1}/{steps}...", end="")
+        print(f"\r{i+1}/{iterations}...", end="")
 
         if fileSaver is not None:
             fileSaver.update()
@@ -53,19 +53,19 @@ def runNoUi(simulation: Simulation, fileSaver: FileSaver, timestep: np.float64, 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--case', required=True)
-    parser.add_argument('-p', '--profile', action='store_true')
-    parser.add_argument('--no_ui', action='store_true', default=False)
-    parser.add_argument('--save', type=str, default=None)
-    parser.add_argument('--steps', type=int, default=None)
+    parser.add_argument('--case', required=True, choices=Cases.CASES.keys(), help='Which scenario to execute')
+    parser.add_argument('--profile', action='store_true', help='Print which functions take the most time')
+    parser.add_argument('--no_ui', action='store_true', default=False, help='Run without ui')
+    parser.add_argument('--save', metavar="FILENAME", type=str, default=None, help='Save to file')
+    parser.add_argument('--iterations', type=int, default=None, help='How many iterations to run for')
     args = parser.parse_args()
 
     if args.no_ui and not args.save:
         print("Error: Not save to file and no UI", file=sys.stderr)
         return
 
-    if not args.no_ui and args.steps is not None:
-        print("Error: Steps doesn't work with UI", file=sys.stderr)
+    if not args.no_ui and args.iterations is not None:
+        print("Error: Iterations doesn't work with UI", file=sys.stderr)
         return
 
     print("Loading derivatives...", end="")
@@ -103,7 +103,7 @@ def main() -> None:
         scalene_profiler.start()
 
     if args.no_ui:
-        runNoUi(simulation, saver, timestep, args.steps)
+        runNoUi(simulation, saver, timestep, args.iterations)
     else:
         runWithUi(simulation, ui, saver, timestep)
 
